dca06f2393a5e7dd0d69385c6053422608f74f60
Jamin Thornsberry <jamin.thorns@gmail.com>
2016-01-18 22:26:19 -0600

    Cleanup

--- portfolio/about/views.py
+++ portfolio/about/views.py
@@ -6,15 +6,8 @@ import os
 
 # Page that displays about information (inherits base.html)
 def about(request):
-    # # Get the path of the about text
-    # current_dir = os.path.dirname(__file__)
-    # about_text_path = os.path.join(current_dir, 'static/txt/about_text.txt')
-
-    # # Get the about text, rendered as markdown
-    # about_text = markdown(open(about_text_path).read())
-
     birthday = datetime.strptime(settings.BIRTHDAY, '%m/%d/%Y')
-    age = int((datetime.today() - birthday).days / 365) # Close enough
+    age = int((datetime.today() - birthday).days / 365.2425) # Close enough
 
     context = {
         'age': age,
